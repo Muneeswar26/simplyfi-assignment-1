9524f05be293ffa4146407d4af444d73375ca27f
Muneeswar26 <msreddy17052003@gmail.com>
2024-08-31 07:49:49 +0530

    code work

---
+++ src/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="container">
+      <div className="diamond">
+        <div className="inner-content">
+          <span className="title">HTML <span className="and">&</span>  CSS</span> <br />
+          <span className="subtitle">design and build websites</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
